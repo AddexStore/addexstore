235e761f2c47233c52cd235cdfafa671c5cf93f9
ebru <ebrueroluduma@gmail.com>
2026-06-09 11:55:11 +0530

    added space for addex store

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -54,9 +54,9 @@ export default function Navbar() {
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-14 lg:h-20">
-          <Link to="/" className="flex-shrink-0">
-            <span className="font-playfair-display text-lg sm:text-xl lg:text-2xl font-bold" style={{ color: '#C6A972' }}>
-              AddexStores
+          <Link to="/" className="flex-shrink-0 mr-4 lg:mr-10">
+            <span className="font-playfair-display text-lg sm:text-xl lg:text-2xl font-bold whitespace-nowrap" style={{ color: '#C6A972' }}>
+              Addex Store
             </span>
           </Link>
 
